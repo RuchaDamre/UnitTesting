8430a0791a1c17cf1b8240dffe2228bb2c073b9d
RuchaDamre <ruchadamre2000@gmail.com>
2022-08-09 14:28:54 +0530

    Testing

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+static void Main(String [] args)
+{
+
+int num = Add(4,5);
+
+Console.WriteLine(isOdd(5));
+Console.WriteLine(num);
+
+}
+public static int Add(int x, int y)
+{
+return x+y;
+
+}
+public static bool isOdd(int value)
+{
+return value%2==1;
+
+}
+
+}
